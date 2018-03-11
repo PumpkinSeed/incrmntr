cdfbaacc98e32625e83e0dca9496fe14ce9fa8ef
PumpkinSeed <qwer.kocka@gmail.com>
2018-03-11 16:15:56 +0100

    Add changes in the example to fix tmpfail error

--- examples/main.go
+++ examples/main.go
@@ -4,14 +4,26 @@ import (
 	"fmt"
 
 	"github.com/PumpkinSeed/incrmntr"
+	"github.com/couchbase/gocb"
 )
 
 func main() {
 	inc := incrmntr.New("couchbase://localhost", "increment", "", 999999999999999, 1)
 	for i := 0; i < 20000; i++ {
 		err := inc.Add("test")
-		if err != nil {
+		if err == gocb.ErrTmpFail {
+		Loop:
+			for {
+				err := inc.Add("test")
+				if err == nil {
+					break Loop
+				}
+			}
+			continue
+		}
+		if err != gocb.ErrTmpFail && err != nil {
 			fmt.Println(err)
+			// 3216
 		}
 	}
 }
