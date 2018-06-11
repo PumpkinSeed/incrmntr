69388bc18ac43c037b3881bda9f7f7d003a67a7c
PumpkinSeed <qwer.kocka@gmail.com>
2018-06-11 23:34:10 +0200

    Add pprof

---
+++ profile/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/PumpkinSeed/incrmntr"
+	"github.com/couchbase/gocb"
+	"github.com/pkg/profile"
+)
+
+const iterationAdd = 1000
+const iterationConAndAdd = 50
+
+func main() {
+	var standalone bool
+	var memProf bool
+	flag.BoolVar(&standalone, "standalone", false, "Standalone mode")
+	flag.BoolVar(&memProf, "mem-profile", false, "Mem profile")
+	flag.Parse()
+
+	if memProf {
+		defer profile.Start(profile.MemProfile, profile.ProfilePath("."), profile.NoShutdownHook).Stop()
+	} else {
+		defer profile.Start(profile.CPUProfile, profile.ProfilePath("."), profile.NoShutdownHook).Stop()
+	}
+
+	if standalone {
+		fmt.Println(standaloneAdd())
+		return
+	}
+
+	fmt.Println(add())
+	return
+}
+
+func standaloneAdd() error {
+	for i := 0; i < iterationConAndAdd; i++ {
+		err := incrmntr.Add(
+			"couchbase://localhost",
+			gocb.PasswordAuthenticator{
+				Username: "Administrator",
+				Password: "password",
+			},
+			"increment",
+			"",
+			"test2",
+		)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func add() error {
+	cluster, err := gocb.Connect("couchbase://localhost")
+	if err != nil {
+		return fmt.Errorf("error connecting to the cluster: %s", err.Error())
+	}
+	cluster.Authenticate(gocb.PasswordAuthenticator{
+		Username: "Administrator",
+		Password: "password",
+	})
+	//inc := New("couchbase://cb1,cb2", "increment", "", 999, 1)
+	inc, err := incrmntr.New(cluster, "increment", "", 999, 1)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < iterationAdd; i++ {
+		err := inc.Add("test")
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
